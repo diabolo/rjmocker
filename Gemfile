8d5ce7a1bf564eaa20b44378a597b788dcd548dc
Andrew Premdas <apremdas@yahoo.co.uk>
2010-12-17 20:15:40 +0000

    fixed Gemfile, first stepdef will debug

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source "http://rubygems.org"
 
 group :development, :test do
-  gem "rspec", "~> 2.1.0"
-  gem "cucumber", ">= 0"
+  gem "rspec"
+  gem "cucumber"
   gem "jeweler", "~> 1.5.1"
   gem "rcov", ">= 0"
   if (RUBY_VERSION < "1.9") 
--- features/step_definitions/start_steps.rb
+++ features/step_definitions/start_steps.rb
@@ -1,8 +1,10 @@
 When /^I start rjmocker$/ do
   debugger
+  RJMocker.start
   pending # express the regexp above with the code you wish you had
 end
 
 Then /^rjmocker should be started$/ do
+  RJMocker.status.should == "started"
   pending # express the regexp above with the code you wish you had
 end
